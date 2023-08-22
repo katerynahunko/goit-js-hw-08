49b8bcc708f5ae75cd0cc8381c3ad0bc2ddea498
your_space <katerinakaterina182@gmail.com>
2023-08-22 05:39:57 +0300

    2+

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const videoPlayer = new Player(iframe);
+const localStorageKey = 'videoplayer-current-time';
+
+const saveTime = throttle((time) => {
+    localStorage.setItem(localStorageKey, JSON.stringify(time));
+}, 1000);
+
+videoPlayer.on('timeupdate', (data) => {
+    const time = data.seconds;
+    saveTime(time);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const time = JSON.parse(localStorage.getItem(localStorageKey));
+    if (time) {
+        videoPlayer.setCurrentTime(time);
+    }
+});
