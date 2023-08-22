89a2f7cec20a52d01dcb8d04c43d3db39d5cddc4
your_space <katerinakaterina182@gmail.com>
2023-08-22 04:30:27 +0300

    1+

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+const markupGallery = galleryItems.map(
+  ({ preview, original, description }) => `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+     <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`
+);
+
+gallery.insertAdjacentHTML("beforeend", markupGallery.join(""));
+
+const ul = document.querySelector('.gallery');
+ul.style.listStyleType = 'none';
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionPosition: "bottom",
+  captionsData: "alt",
+  captionDelay: 250,
+});
